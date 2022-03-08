b2662cbe6ab71c4205a7c259ca5e431bf06ad65f
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-07 21:25:51 -0600

    add journal-entry viewmodel

---
+++ frontend/src/app/viewmodels/JournalEntry.ts
@@ -0,0 +1,8 @@
+export class JournalEntry {
+    public author: number;
+    public date: string;
+    public duration: number;
+    public description: string;
+    public category: string;
+    public title: string;
+}
