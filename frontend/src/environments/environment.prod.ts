65ba312f72f9622029c4eff25f69e3cdab910ecb
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-07 21:10:17 -0600

    add boilerplate code

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  title: 'Local Environment',
+  server_api_base_url: 'http://localhost:8000'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  title: 'Local Environment',
+  server_api_base_url: 'http://localhost:8000'
+};
