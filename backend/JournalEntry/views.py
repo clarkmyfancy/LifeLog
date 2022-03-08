9d00392d9e91a00d5b40fad336bb979845794c19
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-08 09:41:21 -0600

    add auth and permission requirements for viewing journal entries

--- backend/JournalEntry/views.py
+++ backend/JournalEntry/views.py
@@ -1,12 +1,11 @@
 from rest_framework import viewsets, permissions
+from rest_framework.authentication import TokenAuthentication
 
 from JournalEntry.entry_serializer import JournalEntrySerializer
 from JournalEntry.models import JournalEntry
 
-# Create your views here.
 class JournalEntryViewSet(viewsets.ModelViewSet):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    # def post(self, request, format=None):
+    authentication_classes = [TokenAuthentication,]
+    permission_classes = [permissions.IsAuthenticated]
