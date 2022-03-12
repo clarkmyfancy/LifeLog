d348c8cf67f013dbc473421d9ddbf5ab9031e1b9
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-12 16:56:45 -0600

    update datetime

--- backend/JournalEntry/models.py
+++ backend/JournalEntry/models.py
@@ -1,11 +1,12 @@
 import datetime
 from django.db import models
+from django.utils import timezone
 
 from User.models import User
 
 class JournalEntry(models.Model):
     author = models.ForeignKey(to=User, default=1, on_delete=models.DO_NOTHING)
-    date = models.DateTimeField(default=datetime.datetime)
+    date = models.DateTimeField(default=timezone.now())
     duration = models.PositiveIntegerField()
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500, blank=True)
