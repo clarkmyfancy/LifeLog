075b2091f5f6bef1e1ee5d8f1f47e046e4ea5848
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-12 22:13:11 -0600

    remove title field

--- backend/JournalEntry/models.py
+++ backend/JournalEntry/models.py
@@ -8,10 +8,9 @@ class JournalEntry(models.Model):
     author = models.ForeignKey(to=User, default=1, on_delete=models.DO_NOTHING)
     date = models.DateTimeField(default=timezone.now)
     duration = models.PositiveIntegerField()
-    title = models.CharField(max_length=50)
     description = models.CharField(max_length=500, blank=True)
     category = models.CharField(max_length=50, default="", blank=True)
     related_goal = models.ForeignKey(to=Goal, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.title
+        return self.description
