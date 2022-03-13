f8286182c9226335be41b0e0924dad47c9a1b24d
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-12 21:17:14 -0600

    add related_goal foreign key and clean up date

--- backend/JournalEntry/models.py
+++ backend/JournalEntry/models.py
@@ -1,16 +1,17 @@
-import datetime
 from django.db import models
 from django.utils import timezone
 
 from User.models import User
+from Goals.models import Goal
 
 class JournalEntry(models.Model):
     author = models.ForeignKey(to=User, default=1, on_delete=models.DO_NOTHING)
-    date = models.DateTimeField(default=timezone.now())
+    date = models.DateTimeField(default=timezone.now)
     duration = models.PositiveIntegerField()
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=500, blank=True)
     category = models.CharField(max_length=50, default="", blank=True)
+    related_goal = models.ForeignKey(to=Goal, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
