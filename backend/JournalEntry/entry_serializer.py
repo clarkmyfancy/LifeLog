fb57e72cdf1e276de4f8f7eb26ec26b9640ef59c
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-12 21:17:26 -0600

    expose related_goal at the api

--- backend/JournalEntry/entry_serializer.py
+++ backend/JournalEntry/entry_serializer.py
@@ -12,4 +12,5 @@ class JournalEntrySerializer(serializers.HyperlinkedModelSerializer):
             'title', 
             'description',
             'category',
+            'related_goal'
         ]
