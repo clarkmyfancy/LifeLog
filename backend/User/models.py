35e52f1fbd7e5d1e5e87d5552b13d03af2ce40f3
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-23 12:17:32 -0500

    remove name field

--- backend/User/models.py
+++ backend/User/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50, default="temp")
     username = models.CharField(max_length=50, default="temp_username")
 
     def __str__(self):
