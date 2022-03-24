c6a6d04ca5f02f6b8fb2803aa254c22999483495
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-24 14:27:46 -0500

    replace User model with django.contrib.auth.models User object

--- backend/JournalEntry/models.py
+++ backend/JournalEntry/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
-from User.models import User
 from Goals.models import Goal
 
 class JournalEntry(models.Model):
