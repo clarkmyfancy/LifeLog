d1f172a2408f63b94c5cfe44e2153a77fba83f4b
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-24 18:21:37 -0500

    determine what class belongs to a category

--- frontend/src/app/components/journal-viewer/journal-viewer.component.ts
+++ frontend/src/app/components/journal-viewer/journal-viewer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { JournalEntryService } from '@services/JournalEntry/journal-entry.service';
 import { JournalEntry } from '@viewmodels/JournalEntry';
+import { Label } from '@viewmodels/Label';
 
 @Component({
 	selector: 'journal-viewer',
@@ -21,6 +22,33 @@ export class JournalViewerComponent implements OnInit {
 	private populateEntries(): void {
 		this.journalService.getEntries()
 			.subscribe(entries => this.entries = entries);
+	}
+
+
+	// labels: Label[] = [new Label()];
+	private createLabels(): any[] {
+		return [
+			{ name: "mental", color: "lightblue" } ,
+			{ name: "physical", color: "lightorange" },
+			{ name: "personal", color: "blue" },
+			{ name: "family", color: "pink" },
+			{ name: "job", color: "green" },
+			{ name: "business", color: "purple" },
+			{ name: "financial", color: "lightgreen" }
+			
+		]
+	}
+	
+	
+	public generateClassFor(category: string): string {
+		var classes = "label_wrapper ";
+		let label_to_color = this.createLabels();
+		label_to_color.forEach(label => {
+			if (label.name == category) {
+				classes = classes.concat(label.color);
+			}
+		});
+		return classes;
 	}
 
 }
