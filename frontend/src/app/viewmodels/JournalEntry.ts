1b7b8268a916b50d28b43ff84fc202eb062a7317
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-24 17:39:14 -0500

    add labels field

--- frontend/src/app/viewmodels/JournalEntry.ts
+++ frontend/src/app/viewmodels/JournalEntry.ts
@@ -4,5 +4,7 @@ export class JournalEntry {
     public duration: number;
     public description: string;
     public category: string;
+    // FIXME: does there really need to be this many fields on this object?
+    public labels: string[];
     public title: string;
 }
