4a42b4334d98ad901abd3df6c0d019870f8ee41c
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-24 18:20:01 -0500

    modify label list to match what's on mindmap

--- frontend/src/app/services/Label/label.service.ts
+++ frontend/src/app/services/Label/label.service.ts
@@ -7,18 +7,15 @@ export class LabelService {
 
     constructor() { }
 
-    // FIXME: there is a better place for this (2022-03-12)
-    getLabels(): string[] {
+    public getLabels(): string[] {
         return [
-            'Spiritual Life',
-            'Physical Health',
-            'Personal Life',
-            'Key Relationships',
-            'Career',
-            'Business',
-            'Finances',
-            'Skating',
-            'Software Dev'
+            'mental',
+            'physical',
+            'personal',
+            'family',
+            'job',
+            'business',
+            'financial',
         ];
     }
 }
