b6edcbe508dcd825e8fae6d6df4e3141820ea79a
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-25 14:08:40 -0500

    add add logic to determine which filter has been pressed

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { LabelService } from '@services/Label/label.service';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor(private filterService: LabelService) { }
+
+    filters: string[];
+    selectedFilter: string;
+
+    ngOnInit(): void {
+        this.filters = this.getFilters();
+    }
+
+    private getFilters(): string[] {
+        return this.filterService.getLabels();
+    }
+
+    public a_filter_was_clicked(choice: string): void {
+        this.selectedFilter = choice;
+    }
+
+    currentlySelectedFilter(chosenFilter: any): boolean {
+        return chosenFilter == this.selectedFilter;
+    }
 
 }
