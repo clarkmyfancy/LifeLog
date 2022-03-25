9fe7c3b56c9c10cb1348fb39f834e5628fa06aff
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-25 14:10:49 -0500

    render different list of journal entries based on what filter is selected

--- frontend/src/app/components/journal-viewer/journal-viewer.component.ts
+++ frontend/src/app/components/journal-viewer/journal-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 import { JournalEntryService } from '@services/JournalEntry/journal-entry.service';
 import { JournalEntry } from '@viewmodels/JournalEntry';
@@ -9,11 +9,31 @@ import { Label } from '@viewmodels/Label';
 	templateUrl: './journal-viewer.component.html',
 	styleUrls: ['./journal-viewer.component.scss']
 })
-export class JournalViewerComponent implements OnInit {
+export class JournalViewerComponent implements OnInit, OnChanges {
+
+	@Input() journalEntryType = '';
 
 	constructor(private journalService: JournalEntryService) { }
 
 	entries: JournalEntry[] = [];
+	entriesToDisplay: JournalEntry[] = [];
+
+	// ngOnChanges(changes: SimpleChanges): void {
+
+	// }
+
+	ngOnChanges(changes: SimpleChanges) {
+		for (const propName in changes) {
+			if (propName == "journalEntryType") {
+				const filter_choice = changes[propName];
+				if (!filter_choice.isFirstChange()) {
+					const new_filter_ugly = JSON.stringify(filter_choice.currentValue);
+					const new_filter = JSON.parse(new_filter_ugly);
+					this.entriesToDisplay = this.filter_entries_to_display_based_on(new_filter);
+				}
+			}
+		}
+	}
 
 	ngOnInit(): void {
 		this.populateEntries();
@@ -21,11 +41,31 @@ export class JournalViewerComponent implements OnInit {
 
 	private populateEntries(): void {
 		this.journalService.getEntries()
-			.subscribe(entries => this.entries = entries);
+			.subscribe(entries => {
+				this.entries = this.sortDescending(entries)
+				this.entriesToDisplay = this.entries;
+			});
 	}
 
+	private filter_entries_to_display_based_on(new_filter: string): JournalEntry[] {
+		return this.entries.filter(element => element.category == new_filter);
+	}
 
-	// labels: Label[] = [new Label()];
+	private sortDescending(entries: JournalEntry[]): JournalEntry[] {
+		var sorted_entries = entries.sort((a, b) => {
+			if (a.date > b.date) {
+				return -1;
+			}
+			else if (b.date > a.date) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		});
+		return sorted_entries;
+	}
+
 	private createLabels(): any[] {
 		return [
 			{ name: "mental", color: "lightblue" } ,
@@ -39,7 +79,6 @@ export class JournalViewerComponent implements OnInit {
 		]
 	}
 	
-	
 	public generateClassFor(category: string): string {
 		var classes = "label_wrapper ";
 		let label_to_color = this.createLabels();
@@ -50,5 +89,6 @@ export class JournalViewerComponent implements OnInit {
 		});
 		return classes;
 	}
+
 
 }
