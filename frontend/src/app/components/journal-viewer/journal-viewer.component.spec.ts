37de4782538ecd4549d5f49ccfe917f819164e34
Jack Conrad Clark <clark.jack0@gmail.com>
2022-03-29 18:14:54 -0500

    fix broken tests

--- frontend/src/app/components/journal-viewer/journal-viewer.component.spec.ts
+++ frontend/src/app/components/journal-viewer/journal-viewer.component.spec.ts
@@ -1,25 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JournalViewerComponent } from './journal-viewer.component';
 
 describe('JournalViewerComponent', () => {
-  let component: JournalViewerComponent;
-  let fixture: ComponentFixture<JournalViewerComponent>;
+    let component: JournalViewerComponent;
+    let fixture: ComponentFixture<JournalViewerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ JournalViewerComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [HttpClientModule],
+            declarations: [ JournalViewerComponent ]
+        })
+        .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JournalViewerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(JournalViewerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
