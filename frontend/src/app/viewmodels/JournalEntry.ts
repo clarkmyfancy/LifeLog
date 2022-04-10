e3bfbc04899b786a56be3371853adf7afc158df2
Jack Conrad Clark <clark.jack0@gmail.com>
2022-04-09 22:07:15 -0500

    add fields for category, topic area, and subtopic area

--- frontend/src/app/viewmodels/JournalEntry.ts
+++ frontend/src/app/viewmodels/JournalEntry.ts
@@ -1,12 +1,15 @@
 import { Label } from "@viewmodels/Label";
 
 export class JournalEntry {
+
+    public category: string;
+    public topicArea: string;
+    public subtopicArea: string;
+
     public author: number;
     public date: string;
     public duration: number;
     public description: string;
-    public category: string;
     // FIXME: does there really need to be this many fields on this object?
-    public labels: Label[];
     public title: string;
 }
