44fefbb8eb5e8acc9e06f2a74b0058f887c76355
Jack Conrad Clark <clark.jack0@gmail.com>
2022-04-13 21:12:40 -0500

    remove filters from dashboard

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -11,32 +11,32 @@ export class DashboardComponent implements OnInit {
 
     constructor(private filterService: LabelService) { }
 
-    filters: string[];
-    selectedFilter: string;
+    // filters: string[];
+    // selectedFilter: string;
 
     ngOnInit(): void {
-        this.filters = this.getFilters();
-        this.selectedFilter = "";
+        // this.filters = this.getFilters();
+        // this.selectedFilter = "";
     }
 
-    private getFilters(): string[] {
-        return this.filterService.getLabels();
-    }
+    // private getFilters(): string[] {
+    //     return this.filterService.getLabels();
+    // }
 
-    public a_filter_was_clicked(choice: string): void {
-        this.selectedFilter = choice;
-    }
+    // public a_filter_was_clicked(choice: string): void {
+    //     this.selectedFilter = choice;
+    // }
 
-    public clearSelectedFilter(): void {
-        this.selectedFilter = "";
-    }
+    // public clearSelectedFilter(): void {
+    //     this.selectedFilter = "";
+    // }
 
-    currentlySelectedFilter(chosenFilter: any): boolean {
-        return chosenFilter == this.selectedFilter;
-    }
+    // currentlySelectedFilter(chosenFilter: any): boolean {
+    //     return chosenFilter == this.selectedFilter;
+    // }
 
-    nothingToFilter(): boolean {
-		return this.selectedFilter == "";
-	}
+    // nothingToFilter(): boolean {
+	// 	return this.selectedFilter == "";
+	// }
 
 }
