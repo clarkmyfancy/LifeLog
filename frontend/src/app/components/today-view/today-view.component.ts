d97ce1dd033f8190ebc56a19c21ce7d78090b78b
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-12 13:07:39 -0500

    use the quote service to pull quotes

--- frontend/src/app/components/today-view/today-view.component.ts
+++ frontend/src/app/components/today-view/today-view.component.ts
@@ -1,15 +1,27 @@
+import { QuoteService } from '@services/Quote/quote.service';
+
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '@viewmodels/Quote';
 
 @Component({
-  selector: 'today',
-  templateUrl: './today-view.component.html',
-  styleUrls: ['./today-view.component.scss']
+	selector: 'today',
+	templateUrl: './today-view.component.html',
+	styleUrls: ['./today-view.component.scss']
 })
 export class TodayviewComponent implements OnInit {
 
-  constructor() { }
+	quote_of_the_day: Quote;
 
-  ngOnInit(): void {
-  }
+  	constructor(
+		private quoteService: QuoteService
+	) { }
+
+	ngOnInit(): void {
+		this.populateQuote();
+	}
+
+	public populateQuote(): void {
+		this.quote_of_the_day = this.quoteService.getRandomQuote();
+	}
 
 }
