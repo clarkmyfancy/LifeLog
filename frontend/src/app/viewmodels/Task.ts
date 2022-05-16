dbed8e1770621271326f9b7afcd96b2642923508
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-16 11:01:41 -0500

    create a Task object schema

---
+++ frontend/src/app/viewmodels/Task.ts
@@ -0,0 +1,8 @@
+export class Task {
+    public isOnSuccessList: boolean;
+    public title: string;
+    public isUrgent: boolean;
+    public isImportant: boolean;
+    public dueDate: Date;
+    public datetimeCompletedOn? : Date;
+}
