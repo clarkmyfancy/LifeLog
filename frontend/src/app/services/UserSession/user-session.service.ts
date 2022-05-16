ddf2bc08358fc9d4d824dad8ba7a9719888b5d30
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-16 11:08:03 -0500

    add skeleton usersession service

---
+++ frontend/src/app/services/UserSession/user-session.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserSessionService {
+
+  constructor() { }
+
+  // will store credentials about the logged in user
+}
