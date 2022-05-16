e59e2f5d0f333d164a6fb9844241a5a461a7e433
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-16 11:02:23 -0500

    add more hardcoded quotes

--- frontend/src/app/services/Quote/quote.service.ts
+++ frontend/src/app/services/Quote/quote.service.ts
@@ -9,11 +9,18 @@ export class QuoteService {
 
   	constructor() { }
 
-	public getRandomQuote() {
+	public getRandomQuote(): Quote{
 		let size = this.QUOTES().length;
 		let random_index = Math.floor(Math.random() * Math.floor(size));
 		return this.QUOTES()[random_index];
 	}
+
+	// // how to order it such that the quote is "random" each day, but if the page is reloaded on the same day, 
+	// 	// the quote is the same
+
+	// public getQuoteBasedOnDayOfMonth(): void {
+	// 	console.log(new Date());
+	// }
 
 	private QUOTES():  Quote[]{
 		return [
@@ -27,7 +34,23 @@ export class QuoteService {
 			},
 			{
 				text: "Secrets of the world are hidden behind cliches",
-				author: "Unknown"
+				author: "unknown"
+			},
+			{
+				text: "Self esteem is just the reputation that you have with yourself. You'll always know.",
+				author: "Naval Ravikant"
+			},
+			{
+				text: "God will not have his work made manifest by cowards.",
+				author: "Ralph Waldo Emerson"
+			},
+			{
+				text: "If you chase two rabbits, you will not catch either one.",
+				author: "Russian proverb"
+			},
+			{
+				text: "Your work works on you more than you work on it",
+				author: "unknown"
 			},
 		];
 	}
