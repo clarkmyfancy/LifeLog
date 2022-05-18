2d0d7c0df558a7e3817a679f0d23853ba11e1e6b
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-18 09:48:10 -0500

    add http client code to retrieve tasks from backend

--- frontend/src/app/services/Task/task.service.ts
+++ frontend/src/app/services/Task/task.service.ts
@@ -1,12 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 import { Task } from '@viewmodels/Task';
+
 @Injectable({
 	providedIn: 'root'
 })
 export class TaskService {
 
-	constructor() { }
+	constructor(
+		private client: HttpClient,
+	) { }
+
+	private api_url = '/tasks/';
+
+
+	getTasks(): Observable<Task[]> {
+        const request = this.client.get<Task[]>(this.api_url)
+            .pipe(
+                catchError(this.handleError<Task[]>('getTasks', []))
+            );
+        return request;
+    }
+
+	createTask(entry: Task): Observable<any> {
+        const request = this.client.post<Task>(this.api_url, entry)
+            .pipe(
+                catchError(this.handleError<Task>('createTask'))
+            );
+        return request;
+    }
+
+    private handleError<T>(operation="operation", result?: T) {
+        return (error: any): Observable<T> => {
+
+        console.error(error); 
+        // Let the app keep running by returning an empty result.
+        return of(result as T);
+        };
+    }
+
+
+
+
 
 	public getImportantAndUrgentTasks(): Task[] {
 		return this.TASKS().filter(task => task.isUrgent && task.isImportant);
