018478d6fff871e1562a364ed639bb207555c845
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-19 12:00:02 -0500

    add import to get test to pass

--- frontend/src/app/services/Task/task.service.spec.ts
+++ frontend/src/app/services/Task/task.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { TaskService } from './task.service';
@@ -6,7 +7,9 @@ describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+  });
     service = TestBed.inject(TaskService);
   });
 
