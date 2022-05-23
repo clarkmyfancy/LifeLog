accfe3fa9a9a4d4b13966d5c0f7a5366f1525588
Jack Conrad Clark <clark.jack0@gmail.com>
2022-05-23 14:48:06 -0500

    add function to mark task as complete and emit subjects to subscribers about that

--- frontend/src/app/services/Task/task.service.ts
+++ frontend/src/app/services/Task/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 import { Task } from '@viewmodels/Task';
 
@@ -16,6 +16,15 @@ export class TaskService {
 
 	private api_url = '/tasks/';
 
+    taskChanged: Subject<Task> = new Subject<Task>();
+
+    createTask(entry: Task): Observable<any> {
+        const request = this.client.post<Task>(this.api_url, entry)
+            .pipe(
+                catchError(this.handleError<Task>('createTask'))
+            );
+        return request;
+    }
 
 	getTasks(): Observable<Task[]> {
         const request = this.client.get<Task[]>(this.api_url)
@@ -25,20 +34,23 @@ export class TaskService {
         return request;
     }
 
-	createTask(entry: Task): Observable<any> {
-        const request = this.client.post<Task>(this.api_url, entry)
+    markTaskAsCompleted(task: Task): Observable<Task> {
+        task.actual_completion_datetime = new Date();
+        // console.log(task);
+        const urlToUniquelyIdentifyObject = this.api_url + task.id;
+        const request = this.client.put<Task>(urlToUniquelyIdentifyObject, task)
             .pipe(
-                catchError(this.handleError<Task>('createTask'))
+                catchError(this.handleError<Task>('markTaskAsCompleted', task))
             );
+        this.taskChanged.next(task);
         return request;
     }
 
     private handleError<T>(operation="operation", result?: T) {
         return (error: any): Observable<T> => {
-
-        console.error(error); 
-        // Let the app keep running by returning an empty result.
-        return of(result as T);
+            console.error(error); 
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
         };
     }
 }
